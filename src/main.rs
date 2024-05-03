3f486bd29f166c4a24393bcd3d4640719733f64a
GS-Mafra <117572491+GS-Mafra@users.noreply.github.com>
2024-05-02 21:32:12 -0300

    ping

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,30 @@
-use tokio::net::TcpListener;
+use tokio::{
+    io::{AsyncReadExt, AsyncWriteExt},
+    net::{TcpListener, TcpStream},
+};
 
 #[tokio::main]
-async fn main() {
+async fn main() -> anyhow::Result<()> {
     let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
 
     loop {
         match listener.accept().await {
             Ok((stream, _)) => {
-                println!("accepted new connection")
+                tokio::spawn(async move {
+                    handle_connection(stream)
+                        .await
+                        .inspect_err(|e| eprintln!("{e}"))
+                });
             }
             Err(e) => {
-                println!("error: {}", e);
+                eprintln!("error: {}", e);
             }
         }
     }
+}
+
+async fn handle_connection(mut stream: TcpStream) -> anyhow::Result<()> {
+    // stream.read()
+    stream.write_all(b"+PONG\r\n").await?;
+    Ok(())
 }
