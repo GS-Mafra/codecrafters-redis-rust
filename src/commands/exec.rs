9528d85b37b3d86e4449af1f7b999b927931c90c
GS-Mafra <117572491+GS-Mafra@users.noreply.github.com>
2024-06-22 03:21:36 -0300

    exec

---
+++ src/commands/exec.rs
@@ -0,0 +1,15 @@
+use anyhow::ensure;
+
+use super::IterResp;
+
+pub struct Exec;
+
+impl Exec {
+    pub(super) fn parse(mut i: IterResp) -> anyhow::Result<()> {
+        ensure!(
+            i.next().is_none(),
+            "ERR wrong number of arguments for 'multi' command"
+        );
+        Ok(())
+    }
+}
